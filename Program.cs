a36f11e03588afb349e199b785ebae91f877c5eb
Isildir <Xp250701>
2018-06-06 00:59:08 +0200

    Program to Compare two dates

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Program
+{
+    public class Program
+    {
+        public static string BAD_ORDER = "Second date must be earlier than first one";
+
+        public static string CompareDates(DateTime firstDate, DateTime secondDate)
+        {
+            if (firstDate.CompareTo(secondDate) >= 0)
+                return BAD_ORDER;
+            else if (firstDate.Month == secondDate.Month && firstDate.Year == secondDate.Year)
+                return string.Format("{0} - {1}", firstDate.Day.ToString("00"), secondDate.ToShortDateString());
+            else if (firstDate.Year == secondDate.Year)
+                return string.Format("{0}.{1} - {2}", firstDate.Day.ToString("00"), firstDate.Month.ToString("00"), secondDate.ToShortDateString());
+            else
+                return string.Format("{0} - {1}", firstDate.ToShortDateString(), secondDate.ToShortDateString());
+        }
+
+        static void Main(string[] args)
+        {
+            string result;
+
+            if (args.Count() != 2)
+                result = "Invalid number of parameters";
+            else if (DateTime.TryParse(args[0], out DateTime firstDate) && DateTime.TryParse(args[1], out DateTime secondDate))
+                result = CompareDates(firstDate, secondDate);
+            else
+                result = "Invalid arguments formating";
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        [TestCase("2016/1/1", "2016.01.12")]
+        [TestCase("2016.01.01", "2016.11.12")]
+        [TestCase("1-01-2016", "8-12-2017")]
+        public void CompareDatas_WhenCalled_ReturnsStringContainingSecondDate(string firstValue, string secondValue)
+        {
+            DateTime firstDate = DateTime.Parse(firstValue);
+            DateTime secondDate = DateTime.Parse(secondValue);
+            
+            string result = Program.Program.CompareDates(firstDate, secondDate);
+            
+            Assert.That(result, Does.Contain(secondDate.ToShortDateString()).IgnoreCase);
+        }
+
+        [Test]
+        [TestCase("01.01.2016", "01.01.2016")]
+        [TestCase("1.11.2016", "12.01.2016")]
+        public void CompareDatas_WhenCalled_ReturnsBadOrderValue(string firstValue, string secondValue)
+        {
+            DateTime firstDate = DateTime.Parse(firstValue);
+            DateTime secondDate = DateTime.Parse(secondValue);
+            
+            string result = Program.Program.CompareDates(firstDate, secondDate);
+            
+            Assert.That(result, Is.EqualTo(Program.Program.BAD_ORDER).IgnoreCase);
+        }
+    }
+}
